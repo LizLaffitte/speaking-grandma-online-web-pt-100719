83559606360056d986caff2ca86b70c25aaf5405
Liz Laffitte <liz.kossakowski@gmail.com>
2019-10-07 21:56:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,10 @@
 def speak_to_grandma(phrase)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
-  elsif phrase == phrase.upcase
-    response = "NO, NOT SINCE 1938!"
-
   if phrase == "I LOVE YOU GRANDMA!"
     response = "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    response = "NO, NOT SINCE 1938!"
   else 
     response = "HUH?! SPEAK UP, SONNY!"
   end
